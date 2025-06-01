d06c0d2069284cc5468eae8f478eb78fa0fa56fb
Konda Sandeep <139792713+SandeepOCT@users.noreply.github.com>
2025-06-01 17:37:27 +0530

    ci: configure CI pipeline

--- release.config.js
+++ release.config.js
@@ -5,6 +5,20 @@ module.exports = {
       "@semantic-release/commit-analyzer",
       {
         preset: "conventionalcommits", // using the conventionalcommits preset, default is angular
+        presetConfig: {
+          types: [
+            { type: "feat", section: "Features" },
+            { type: "fix", section: "Bug Fixes" },
+            { type: "build", section: "Dependencies", hidden: false },
+            { type: "chore", section: "Other Changes", hidden: false },
+            { type: "docs", section: "Documentation", hidden: false },
+            { type: "style", section: "Styles", hidden: false },
+            { type: "refactor", section: "Other Changes", hidden: false },
+            { type: "perf", section: "Performance Improvements" },
+            { type: "test", hidden: true },
+            { type: "ci", section: "Other Changes", hidden: false },
+          ],
+        },
       },
     ],
     [
@@ -16,20 +30,6 @@ module.exports = {
     "@semantic-release/release-notes-generator",
     {
       preset: "conventionalcommits",
-      presetConfig: {
-        types: [
-          { type: "feat", section: "Features" },
-          { type: "fix", section: "Bug Fixes" },
-          { type: "build", section: "Dependencies", hidden: false },
-          { type: "chore", section: "Other Changes", hidden: false },
-          { type: "docs", section: "Documentation", hidden: false },
-          { type: "style", section: "Styles", hidden: false },
-          { type: "refactor", section: "Other Changes", hidden: false },
-          { type: "perf", section: "Performance Improvements" },
-          { type: "test", hidden: true },
-          { type: "ci", section: "Other Changes", hidden: false },
-        ],
-      },
     },
     "@semantic-release/npm",
     [
