e4621ba1946761caf6218bb63eff51460509a551
Konda Sandeep <139792713+SandeepOCT@users.noreply.github.com>
2025-06-01 17:39:49 +0530

    ci: configure CI pipeline

--- release.config.js
+++ release.config.js
@@ -27,10 +27,26 @@ module.exports = {
         changelogFile: "CHANGELOG.md",
       },
     ],
-    "@semantic-release/release-notes-generator",
-    {
-      preset: "conventionalcommits",
-    },
+    [
+      "@semantic-release/release-notes-generator",
+      {
+        preset: "conventionalcommits",
+        presetConfig: {
+          types: [
+            { type: "feat", section: "Features" },
+            { type: "fix", section: "Bug Fixes" },
+            { type: "build", section: "Dependencies", hidden: false },
+            { type: "chore", section: "Other Changes", hidden: false },
+            { type: "docs", section: "Documentation", hidden: false },
+            { type: "style", section: "Styles", hidden: false },
+            { type: "refactor", section: "Other Changes", hidden: false },
+            { type: "perf", section: "Performance Improvements" },
+            { type: "test", hidden: true },
+            { type: "ci", section: "Other Changes", hidden: false },
+          ],
+        },
+      },
+    ],
     "@semantic-release/npm",
     [
       "@semantic-release/git",
